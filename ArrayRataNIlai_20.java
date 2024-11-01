bdefb9ced9d6f957ac07cb052d95f69e0851afdf
rojalingga <rojalingga2006@gmail.com>
2024-11-01 09:15:02 +0700

    deklarasi jumlah nilai array berdasarkan input user dan menghitung rata nilai lulus dan tidak lulus

--- ArrayRataNIlai_20.java
+++ ArrayRataNIlai_20.java
@@ -4,11 +4,14 @@ public class ArrayRataNIlai_20 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        int[] nilaiMhs = new int[10];
+        System.out.print("Masukkan jumlah mahasiswa : ");
+        int jmlMhs = sc.nextInt();
+
+        int[] nilaiMhs = new int[jmlMhs];
         double total = 0;
-        double rata2;
-
-
+        double rata2, nilaiLulus = 0, nilaiTidakLulus = 0, rataLulus, rataTidakLulus;
+        int lulus = 0, tidakLulus = 0;
+        
         for (int i = 0; i < nilaiMhs.length; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + (i+1) + ": ");
             nilaiMhs[i] = sc.nextInt();
@@ -18,8 +21,20 @@ public class ArrayRataNIlai_20 {
             total += nilaiMhs[i];
         }
 
-        rata2 = total / nilaiMhs.length;
+        for (int i = 0; i < nilaiMhs.length ; i++) {
+            if (nilaiMhs[i] >= 70) {
+                lulus++;
+                nilaiLulus += nilaiMhs[i];
+            } else if (nilaiMhs[i] < 70) {
+                tidakLulus++;
+                nilaiTidakLulus += nilaiMhs[i];
+            }
+        }
 
-        System.out.println("Rata-rata nilai = " + rata2);
+        rataLulus = nilaiLulus / lulus;
+        rataTidakLulus = nilaiTidakLulus / tidakLulus;
+
+        System.out.println("Rata - rata nilai lulus = " +rataLulus);
+        System.out.println("Rata - rata nilai lulus = " +rataTidakLulus);
     }
 }
