5165c1400467d5ad21b827d9b8aeefa463e299ee
rojalingga <rojalingga2006@gmail.com>
2024-11-01 09:45:33 +0700

    input berupa banyaknya elemen array nilai, isi array, dan sebuah nilai (key) yang ingin dicari

--- SearchNilai_20.java
+++ SearchNilai_20.java
@@ -1,18 +1,34 @@
+import java.util.Scanner;
+
 public class SearchNilai_20 {
     public static void main(String[] args) {
-        int[] arrNilai = {80, 85, 78, 96, 90, 82, 86};
-        int key = 90;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan banyaknya nilai yang akan diinput : ");
+        int banyakNilai = sc.nextInt();
+
+        int[] arrNilai = new int[banyakNilai];
+        int key;
         int hasil = 0;
 
         for (int i = 0; i < arrNilai.length; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" +(i+1)+ " : ");
+            arrNilai[i] = sc.nextInt();
+        }
+
+        System.out.print("Masukkan nilai yang ingin dicari : ");
+        key = sc.nextInt();
+
+        for (int i = 0; i < arrNilai.length; i++) {
             if (key == arrNilai[i]) {
-                hasil = i;
+                hasil = i + 1;
                 break;
             }
         }
 
         System.out.println();
-        System.out.println("Nilai " + key + " ketemu di indeks ke-" + hasil);
+        System.out.println("Nilai " + key + " merupakan nilai mahasiswa ke-" + hasil);
         System.out.println();
     }
 }
